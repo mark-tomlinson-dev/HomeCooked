623308e66628320376d6e114fd088d3a425a2edb
Mark Tomlinson <mark.tomlinson3@gmail.com>
2019-11-02 21:26:02 +1100

    add is_host to devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "/", to: "pages#home", as: "root"
+  get "/", to: "pages#index", as: "root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
