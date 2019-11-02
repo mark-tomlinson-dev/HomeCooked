0f1457260aa6051397bf6227528d3e7d8828bf00
Mark Tomlinson <mark.tomlinson3@gmail.com>
2019-11-02 21:29:56 +1100

    add profile model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'profiles/index'
+  get 'profiles/show'
+  get 'profiles/new'
+  get 'profiles/edit'
+  get 'profiles/create'
+  get 'profiles/update'
+  get 'profiles/destroy'
   devise_for :users
   get "/", to: "pages#index", as: "root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
