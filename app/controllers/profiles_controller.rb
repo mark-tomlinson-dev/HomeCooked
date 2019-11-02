06e8d5426a008b2e49f10351d3fb8723b9dec8c5
Mark Tomlinson <mark.tomlinson3@gmail.com>
2019-11-02 22:29:01 +1100

    allow user to create profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,17 +1,27 @@
 class ProfilesController < ApplicationController
+  before_action :set_profile, only: [:show, :edit, :update, :destroy]
+  # Authentication not required for browsing profiles
+  before_action :authenticate_user!, except: [:show, :index]
+  
   def index
+    @profiles = Profile.all
   end
 
   def show
   end
 
   def new
+    @profile = Profile.new 
   end
 
   def edit
   end
 
   def create
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
+		@profile.save
+		redirect_to @profile
   end
 
   def update
@@ -19,4 +29,16 @@ class ProfilesController < ApplicationController
 
   def destroy
   end
+
+
+  private
+
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end 
+
+  def profile_params
+    params.require(:profile).permit(:about, :first_name, :last_name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'profiles/index'
-  get 'profiles/show'
-  get 'profiles/new'
-  get 'profiles/edit'
-  get 'profiles/create'
-  get 'profiles/update'
-  get 'profiles/destroy'
   devise_for :users
   get "/", to: "pages#index", as: "root"
+  
+  # Profiles routes
+  resources :profiles
+
+ 
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
